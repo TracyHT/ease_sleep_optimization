c7213615f9bf21acccb99bb0ab7304a89a3fabcc
Tracy <131429059+TracyHT@users.noreply.github.com>
2025-04-18 22:58:54 +0700

    Firebase Setup

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -12,8 +12,13 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
+}
+
+plugins {
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
 tasks.register<Delete>("clean") {
